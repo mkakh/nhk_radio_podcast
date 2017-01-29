7d40b62df90339798696eabec9b49e5de1211940
akh <akhs3214@gmail.com>
2017-01-30 07:17:27 +0900

    deleted unneeded command

--- radio.sh
+++ radio.sh
@@ -10,7 +10,7 @@ then
   mv -v ${SAVEPATH}/*.mp3 ${SAVEPATH}/old/
 fi
 URIXML=$(echo "cat /rss/channel/item/enclosure/@url" |xmllint --shell http://www.nhk.or.jp/r-news/podcast/nhkradionews.xml)
-URL=`echo ${URIXML} |grep http| sed -e "s/^.*url=\"\(.*\)\".*$/\1/"`
+URL=`echo ${URIXML} | sed -e "s/^.*url=\"\(.*\)\".*$/\1/"`
 wget -nc -P ${SAVEPATH} ${URL}
 
 SAVEPATH=/home/akh/radio/english
@@ -23,7 +23,7 @@ then
   mv -v ${SAVEPATH}/*.mp3 ${SAVEPATH}/old/
 fi
 URIXML=$(echo "cat /rss/channel/item/enclosure/@url" |xmllint --shell http://www3.nhk.or.jp/rj/podcast/rss/english.xml)
-URL=`echo ${URIXML} |grep http| sed -e "s/^.*url=\"\(.*\)\".*$/\1/"`
+URL=`echo ${URIXML} | sed -e "s/^.*url=\"\(.*\)\".*$/\1/"`
 wget -nc -P ${SAVEPATH} ${URL}
 
 SAVEPATH=/home/akh/radio/russian
@@ -36,7 +36,7 @@ then
   mv -v ${SAVEPATH}/*.mp3 ${SAVEPATH}/old/
 fi
 URIXML=$(echo "cat /rss/channel/item/enclosure/@url" |xmllint --shell http://www3.nhk.or.jp/rj/podcast/rss/russian.xml)
-URL=`echo ${URIXML} |grep http| sed -e "s/^.*url=\"\(.*\)\".*$/\1/"`
+URL=`echo ${URIXML} | sed -e "s/^.*url=\"\(.*\)\".*$/\1/"`
 wget -nc -P ${SAVEPATH} ${URL}
 
 
