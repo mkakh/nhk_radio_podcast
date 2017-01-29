3fe19f324ec18bee00267560becc7641e52181cf
akh <akhs3214@gmail.com>
2017-01-30 07:12:17 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# nhk_radio_podcast
+
+## NHK Radioのpodcastを取得するスクリプト
+
+こちらのサイトを参考にしました
+
+http://takeshich.hatenablog.com/entry/2015/11/23/162139
+
---
+++ radio.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SAVEPATH=/home/akh/radio/japanese
+if [ ! -e ${SAVEPATH} ]
+then
+  mkdir -p ${SAVEPATH}
+fi
+if ls ${SAVEPATH}/*.mp3 > /dev/null 2>&1
+then
+  mv -v ${SAVEPATH}/*.mp3 ${SAVEPATH}/old/
+fi
+URIXML=$(echo "cat /rss/channel/item/enclosure/@url" |xmllint --shell http://www.nhk.or.jp/r-news/podcast/nhkradionews.xml)
+URL=`echo ${URIXML} |grep http| sed -e "s/^.*url=\"\(.*\)\".*$/\1/"`
+wget -nc -P ${SAVEPATH} ${URL}
+
+SAVEPATH=/home/akh/radio/english
+if [ ! -e ${SAVEPATH} ]
+then
+  mkdir -p ${SAVEPATH}
+fi
+if ls ${SAVEPATH}/*.mp3 > /dev/null 2>&1
+then
+  mv -v ${SAVEPATH}/*.mp3 ${SAVEPATH}/old/
+fi
+URIXML=$(echo "cat /rss/channel/item/enclosure/@url" |xmllint --shell http://www3.nhk.or.jp/rj/podcast/rss/english.xml)
+URL=`echo ${URIXML} |grep http| sed -e "s/^.*url=\"\(.*\)\".*$/\1/"`
+wget -nc -P ${SAVEPATH} ${URL}
+
+SAVEPATH=/home/akh/radio/russian
+if [ ! -e ${SAVEPATH} ]
+then
+  mkdir -p ${SAVEPATH}
+fi
+if ls ${SAVEPATH}/*.mp3 > /dev/null 2>&1
+then
+  mv -v ${SAVEPATH}/*.mp3 ${SAVEPATH}/old/
+fi
+URIXML=$(echo "cat /rss/channel/item/enclosure/@url" |xmllint --shell http://www3.nhk.or.jp/rj/podcast/rss/russian.xml)
+URL=`echo ${URIXML} |grep http| sed -e "s/^.*url=\"\(.*\)\".*$/\1/"`
+wget -nc -P ${SAVEPATH} ${URL}
+
+
