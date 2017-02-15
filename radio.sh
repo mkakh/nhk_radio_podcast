353b1e420a915d9cdc4d193724b8455661cf8adf
mkakh <akhs3214@gmail.com>
2017-02-16 08:28:05 +0900

    xargs

--- radio.sh
+++ radio.sh
@@ -4,9 +4,12 @@ set -eu
 SAVEPATH=~/radio
 URIXML=$(echo "cat /rss/channel/item/enclosure/@url" |xmllint --shell http://www.nhk.or.jp/r-news/podcast/nhkradionews.xml)
 URL=`echo ${URIXML} | sed -e "s/^[^\"]*url=\"\([^\"]*\)\".*$/\1/"`
+URLS=${URL}
 while [ "${URL}" != "/ >" ]
   do
-    wget -nc -P ${SAVEPATH} ${URL}
     URIXML=`echo ${URIXML} | sed -e "s/^[^\"]*url=\"[^\"]*\"\(.*$\)/\1/"`
     URL=`echo ${URIXML} | sed -e "s/^[^\"]*url=\"\([^\"]*\)\".*$/\1/"`
+    URLS="${URLS} ${URL}"
   done
+
+  echo ${URLS%\/ >} | xargs -n 1 -P `nproc` wget -nc -P ${SAVEPATH}
